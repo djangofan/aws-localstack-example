9fc7c40ad13b44efc795fd1095709ba6ab5e28e8
Jon Austen <djangofan@gmail.com>
2018-04-14 17:08:08 -0700

    Got it working!

--- create_resources.sh
+++ create_resources.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-AWS_ACCESS_KEY_ID=xxxx
-AWS_SECRET_ACCESS_KEY=xxxx
+
 # command 'aws configure list'
 # command 'aws configure'
+# aws --debug --region us-west-2 --no-sign-request --endpoint-url=http://192.168.99.100:4572 s3 mb s3://mytestbucket
 
-aws --debug --endpoint-url=http://192.168.99.100:4572 s3 mb s3://mytestbucket
+aws --region us-west-2 --no-sign-request --endpoint-url=http://192.168.99.100:4572 s3 mb s3://mytestbucket
 
+aws --no-sign-request --endpoint-url=http://192.168.99.100:4570 sqs create-queue --queue-name MyQueue
 
