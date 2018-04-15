e77e7ca651d4ded7ed61447be82150e16399a3d5
Jon Austen <djangofan@gmail.com>
2018-04-15 15:56:04 -0700

    Got it working!

--- create_resources.sh
+++ create_resources.sh
@@ -4,6 +4,8 @@
 # command 'aws configure'
 # aws --debug --region us-west-2 --no-sign-request --endpoint-url=http://192.168.99.100:4572 s3 mb s3://mytestbucket
 
+# another way to do this:  https://github.com/localstack/awscli-local
+
 aws --region us-west-2 --no-sign-request --endpoint-url=http://192.168.99.100:4572 s3 mb s3://mytestbucket
 
 aws --no-sign-request --endpoint-url=http://192.168.99.100:4570 sqs create-queue --queue-name MyQueue
